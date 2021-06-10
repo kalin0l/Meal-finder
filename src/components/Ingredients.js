015a5ebfdba57c98e2d1186fa408856c6f536df3
kalin0l <aleksiev2494@gmail.com>
2021-06-10 20:12:32 +0300

    updating measures and ingredients

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
 const Ingredients = (props) => {
+    console.log(props[0])
     return (
-
         <div className='ingredients'>
             <h3>Ingredients</h3>
-            <span>1: {props[0]}</span>
-            <span>2: {props[1]}</span>
-            <span>3: {props[2]}</span>
-            <span>4: {props[3]}</span>
-            <span>5: {props[4]}</span>
-            <span>6: {props[5]}</span>
+            {props[0] && props[0].length > 0 ? props[0].map((item, i) => {
+                return <span key={i}>{i + 1}: {item}</span>
+            }) : null}
         </div>
     )
 }
